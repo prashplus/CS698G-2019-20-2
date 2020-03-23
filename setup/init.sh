a3d9af12642d2afefded7251a65f987578b309b6
prashant <prashplus@gmail.com>
2020-03-23 19:38:12 +0530

    step 1

--- setup/init.sh
+++ setup/init.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 sudo sed -i '/asia/d' ./../../etc/hosts
 sudo sed -i '/useast/d' ./../../etc/hosts
-sudo sed -i '1i40.87.3.199 test1useast' /etc/hosts
-sudo sed -i '1i13.67.108.122 test2asia' /etc/hosts
-sudo sed -i '1i52.187.35.97 test3asia' /etc/hosts
+sudo sed -i '1i40.121.156.19 test1useast' /etc/hosts
+sudo sed -i '1i52.230.7.177 test2asia' /etc/hosts
+sudo sed -i '1i52.230.4.24 test3asia' /etc/hosts
 echo "ETC/HOSTS Modified"
 rm -rf CS698G-2019-20-2
 git clone https://github.com/prashplus/CS698G-2019-20-2
 cd CS698G-2019-20-2
 touch hosts
-echo "40.87.3.199" >> hosts
-echo "13.67.108.122" >> hosts
-echo "52.187.35.97" >> hosts
+echo "40.121.156.19" >> hosts
+echo "52.230.7.177" >> hosts
+echo "52.230.4.24" >> hosts
 echo "MPI hostfile Modified"
-mpicxx -o main.out src.cpp
+cd cmake-build-release
+make clean
+make
 echo "Program ready to execute"
